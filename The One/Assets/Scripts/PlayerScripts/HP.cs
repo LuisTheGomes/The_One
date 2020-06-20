4f46da666f7f3457cf57f719aba049bc514b1c5e
malmaboy <58644254+malmaboy@users.noreply.github.com>
2020-06-20 19:43:25 +0100

    New

---
+++ The One/Assets/Scripts/PlayerScripts/HP.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HP : MonoBehaviour
+{
+    public int hp = 100;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            
+            hp --;
+            
+        }
+    }
+}
